54b21d88f1066e686cd0b1f82f051dcad8c8c20e
Sean Hagen <sean.r.hagen@gmail.com>
2020-08-16 16:28:51 -0500

    Add project files.

---
+++ NDSUShortestPath/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using QuickGraph;
+using QuickGraph.Algorithms;
+using QuickGraph.Algorithms.ShortestPath;
+namespace NDSUShortestPath
+{
+    class Program
+    {
+        public static AdjacencyGraph<String, Edge<string>> graph;
+        public static Dictionary<Edge<string>, double> weight;
+        private static string writeTo = ".\\..\\..\\..\\..\\output.csv";
+        private static string readFrom = ".\\..\\..\\..\\..\\GraphCSV.csv";
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Begin Program");
+            Console.WriteLine("Reading from file {0}", readFrom);
+            Console.WriteLine("Save File is {0}", writeTo);
+
+            ReadFile.buildGraph(out graph, out weight, readFrom);
+
+            Func<QuickGraph.Edge<string>, double> edgeCost = QuickGraph.Algorithms.AlgorithmExtensions.GetIndexer(weight);
+            DijkstraShortestPathAlgorithm<string, Edge<string>> dijkstra = new DijkstraShortestPathAlgorithm<string, Edge<string>>(graph, new Func<Edge<String>, double>(edgeCost));
+            writeToFile(edgeCost);        
+        }
+
+        public static void writeToFile(Func<QuickGraph.Edge<string>, double> edgeCost)
+        {
+            string fileToWrite = "";
+            fileToWrite += "How to get to a *building* while spending the least amount of time outside\n Starting Building, Path (multiple Collumns)\n\n";
+            for (int j = 0; j < graph.Vertices.Count(); j++)
+            {
+                fileToWrite += "Starting at " + graph.Vertices.ElementAt(j) + "\n";
+                fileToWrite += computeForVertex(graph.Vertices.ElementAt(j), edgeCost) + "\n";
+            }
+            File.WriteAllText(writeTo, fileToWrite);
+        }
+
+
+        public static string computeForVertex(string vertex, Func<QuickGraph.Edge<string>, double> edgeCost)
+        {
+            string output = "";
+            var tryGetPaths = graph.ShortestPathsDijkstra(edgeCost, vertex);
+            foreach (string vert in graph.Vertices)
+            {
+                output += "To " + vert + ",";
+                IEnumerable<Edge<string>> path;
+                if (tryGetPaths(vert, out path))
+                    foreach (var edge in path)
+                    {
+                        output += edge + ",";
+                    }
+                if (path is null)
+                {
+                    output += "You are already at " + vert;
+                }
+                output += "\n";
+            }
+            return output;
+
+        }
+    }
+}
+
---
+++ NDSUShortestPath/ReadFile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using QuickGraph;
+
+namespace NDSUShortestPath
+{
+    public class ReadFile
+    {
+        public static void buildGraph(out AdjacencyGraph<String, Edge<string>> graph, out Dictionary<Edge<string>, double> weight, string filePath)
+        {
+            graph = new AdjacencyGraph<string, Edge<string>>(true);
+            weight = new Dictionary<Edge<string>, double>();
+            using (var csv = new StreamReader(filePath))
+            {
+                while (!csv.EndOfStream)
+                {
+                    string line = csv.ReadLine();
+                    string[] split = line.Split(',');
+
+                    //This is hard coded, because each line should only have three values
+                    //Vertex one, Vertex two and weight
+                    if (!graph.Vertices.Contains(split[0]))
+                    {
+                        graph.AddVertex(split[0]);
+                    }
+                    if (!graph.Vertices.Contains(split[1]))
+                    {
+                        graph.AddVertex(split[1]);
+                    }
+                    Edge<string> edge = new Edge<string>(split[0], split[1]);
+                    Edge<string> edgeTwo = new Edge<string>(split[1], split[0]);
+                    graph.AddEdge(edge);
+                    graph.AddEdge(edgeTwo);
+                    weight.Add(edge, Double.Parse(split[2]));
+                    weight.Add(edgeTwo, Double.Parse(split[2]));
+                }
+            }
+        }
+    }
+}
